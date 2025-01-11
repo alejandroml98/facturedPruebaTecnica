5618f955e5a858f34952b4e31a2d8fcd497e7b55
Alejandro Martinez <alejandro.martinez@movistar.com.sv>
2025-01-11 16:34:26 -0600

    creación de modelos para tabla TABLEROS y TAREAS

--- commandApp/apis/models.py
+++ commandApp/apis/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Tableros(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=200,null=True)
+    descripcion = models.CharField(max_length=500,null=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'TABLEROS'
+
+class Tareas(models.Model):
+    id = models.IntegerField(primary_key=True)
+    titulo = models.CharField(max_length=200,null=True)
+    descripcion = models.CharField(max_length=500,null=True)
+    estado = models.CharField(max_length=50,null=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    #Llave foranea con tabla "Tableros"
+    id_tablero = models.ForeignKey(Tableros, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'TAREAS'
--- commandApp/commandApp/settings.py
+++ commandApp/commandApp/settings.py
@@ -32,12 +32,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #apps de django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #apps propias
+    "apis"
 ]
 
 MIDDLEWARE = [
